0ab233f69715cfc25053aabfe9a2550311a880e5
Iamanujosh <joshi2017anu@gmail.com>
2025-05-24 20:57:13 +0530

    commited

--- debatehub-frontend/src/pages/loginPage.js
+++ debatehub-frontend/src/pages/loginPage.js
@@ -12,7 +12,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('https://debatehub-1.onrender.com', {
+    const response = await fetch('https://debatehub-1.onrender.com/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
